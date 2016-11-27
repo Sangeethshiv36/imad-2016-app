1221d15602e6f859fdce40a32532f5099addb20f
Sangeethshiv36 <b.sangeethsivan@gmail.com>
2016-11-27 19:05:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ submit.onclick = function(){
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST','http://sangeethshiv36.imad.hasura-app.io/login',true);
+        request.open('POST',window.location.protocol+"//"+window.location.host+"/login",true);
         request.setRequestHeader('Content-Type','application/json');
         request.send(JSON.stringify({username: username, password: password}));
 };
