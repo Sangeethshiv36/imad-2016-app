76a9d55457cdd7e458c8385b7f7cc1cb255ae3fb
Sangeethshiv36 <b.sangeethsivan@gmail.com>
2016-11-27 15:43:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,11 +78,33 @@ app.post('/create-user',function(req,res){
          if(err){
             res.status(500).send(err.toString());
         }else{ 
-            res.send('User Successfully Created : '+username);
+            res.send('User Successfully Created : ' + username);
         }
     });
 });
 
+app.post('/login',function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user" WHERE username = $1 (username,password) VALUES ($1,$2)',[username],function(err,result){
+         if(err){
+            res.status(500).send(err.toString());
+        }else{ 
+            if(result.rows.length===0){
+                res.send(403).send('username/password is invalid');
+            }else{
+            var dbString=result.rows[0].password;
+            var salt=dbString.split('$')[2];
+            var hashedPassword=hash(password,salt);
+            if(hashedPassword === dbString){
+            res.send('credentials correct');
+        }else{
+            res.send(403).send('username/password is invalid');
+        }
+            }
+        }
+    });
+});
 var pool = new Pool(config);
 
 app.get('/test-db',function(req,res){
