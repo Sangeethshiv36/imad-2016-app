502216cd0508ee388946040ec4426862de64a13e
Sangeethshiv36 <b.sangeethsivan@gmail.com>
2016-11-27 18:06:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,25 @@
-console.log('Loaded!');
+var submit = document.getElementById('submit-btn');
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+               console.log('user logged in');
+               alert('Logged in successfully');
+                }else if(request.status===403){
+                    alert('username/password is incorrect');
+                }else if(request.status===500){
+                    alert('Something went wrong');
+                }
+            }
+        };
+        
+        var username = document.getElementbyId('username').value;
+        var password = document.getElementbyId('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST','http://sangeethshiv36.imad.hasura-app.io/login',true);
+        request.setRequestheader('Content-Type','application/json');
+        request.send(JSON.stringify({username:username,password:password}));
+};
